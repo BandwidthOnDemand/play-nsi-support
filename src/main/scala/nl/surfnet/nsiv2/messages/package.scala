74c1a61545304e0c2d2a44235024bb68cbf725a7
Erik Rozendaal <erik@deler.org>
2014-11-20 13:27:35 +0100

    Provide JSON format support.
    Migrated from safnari.

--- src/main/scala/nl/surfnet/nsiv2/messages/package.scala
+++ src/main/scala/nl/surfnet/nsiv2/messages/package.scala
@@ -6,11 +6,37 @@ import javax.xml.datatype.{DatatypeFactory,XMLGregorianCalendar}
 import javax.xml.namespace.QName
 import org.ogf.schemas.nsi._2013._12.services.point2point.P2PServiceBaseType
 import org.joda.time.{DateTime, DateTimeZone}
+import play.api.data.validation.ValidationError
+import play.api.libs.functional._
+import play.api.libs.functional.ContravariantFunctorOps
+import play.api.libs.json._
+import scala.util.Try
 
 package object messages {
   type RequesterNsa = String
   type ConnectionId = String
   type GlobalReservationId = URI
+
+  def valueFormat[T](message: String)(parse: String => Option[T], print: T => String): Format[T] = new Format[T] {
+    override def reads(json: JsValue): JsResult[T] = json match {
+      case JsString(s) => parse(s) match {
+        case Some(t) => JsSuccess(t)
+        case None    => JsError(Seq(JsPath() -> Seq(ValidationError(message, s))))
+      }
+      case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsstring"))))
+    }
+    override def writes(t: T): JsValue = JsString(print(t))
+  }
+
+  implicit val JavaTimeInstantFormat: Format[java.time.Instant] = Format(
+    implicitly[Reads[Long]].map(java.time.Instant.ofEpochMilli),
+    Writes(d => JsNumber(d.toEpochMilli)))
+  implicit val UriFormat: Format[URI] = valueFormat("error.expected.uri")(
+    parse = s => Try(URI.create(s)).toOption,
+    print = _.toASCIIString)
+  implicit val CorrelationIdFormat: Format[CorrelationId] = valueFormat("error.expected.correlationId")(
+    parse = CorrelationId.fromString,
+    print = _.toString)
 
   private[this] val datatypeFactory = DatatypeFactory.newInstance()
   implicit class DateTimeOps(dt: org.joda.time.ReadableDateTime) {

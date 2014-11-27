b5da69b35cc2b9b64c5b9d4bfcebf7084c6ea968
Erik Rozendaal <erik@deler.org>
2014-11-27 12:12:47 +0100

    Handle MTOSI notification consumer WSDL serving.

--- src/main/scala/nl/surfnet/nsiv2/soap/SoapWebService.scala
+++ src/main/scala/nl/surfnet/nsiv2/soap/SoapWebService.scala
@@ -1,5 +1,6 @@
 package nl.surfnet.nsiv2.soap
 
+import java.net.URI
 import play.api.http.ContentTypes
 import play.api.mvc._
 import play.api.mvc.Results._
@@ -8,13 +9,19 @@ import scalax.io.Resource
 
 trait SoapWebService {
 
-  val BaseWsdlFilename: String
+  // FIXME resolve relative paths correctly. The current implementation works for NSI and MTOSI, but that's about it.
+
+  val WsdlPathPrefix: String
+  val WsdlPath: String
+  val WsdlBasename: String
+
+  //val WsdlFilenamePattern = "[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.(wsdl|xsd)"
 
   def serviceUrl: String
 
-  def wsdl(ignore: String) = SoapWebService.serveWsdl(BaseWsdlFilename) { implicit request =>
-    replaceSoapAddress
-  }
+  def wsdl(ignore: String) = serveWsdl(WsdlBasename) { _ => replaceSoapAddress }
+
+  def wsdlOrXsd(name: String) = serveWsdl(name) { _ => identity }
 
   private def replaceSoapAddress(wsdl: String) = {
     wsdl.replaceAll(
@@ -22,27 +29,19 @@ trait SoapWebService {
       s"""<soap:address location="$serviceUrl" />""")
   }
 
-}
-
-object SoapWebService {
-
-  private val WsdlResourcePath = "wsdl/2.0"
-  private val WsdlFilenamePattern = "[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.(wsdl|xsd)"
-
-  def wsdlOrXsd(name: String) = serveWsdl(name){ _ => identity }
-
-  private def serveWsdl(file: String)(transform: RequestHeader => String => String) = Action { implicit request =>
-    readClasspathWsdl(file).map(transform(request)).map { body =>
+  private def serveWsdl(path: String)(transform: RequestHeader => String => String) = Action { implicit request =>
+    readClasspathWsdl(path).map(transform(request)).map { body =>
       Ok(body).as(ContentTypes.XML)
     }.getOrElse {
-      NotFound(s"Resource '$file' not found")
+      NotFound(s"Resource '$path' not found")
     }
   }
 
   private def readClasspathResource(resource: String): Option[String] =
     Try(Resource.fromClasspath(resource).string(scalax.io.Codec.UTF8)).toOption
 
-  private def readClasspathWsdl(name: String): Option[String] =
-    Some(name).filter(_ matches WsdlFilenamePattern).flatMap(name => readClasspathResource(s"$WsdlResourcePath/$name"))
-
+  private def readClasspathWsdl(name: String): Option[String] = {
+    val resolved = Try(URI.create(WsdlPath).resolve(name).toString).toOption
+    resolved.filterNot(_ contains "..").filterNot(_ startsWith "/").flatMap(name => readClasspathResource(s"$WsdlPathPrefix/$name"))
+  }
 }

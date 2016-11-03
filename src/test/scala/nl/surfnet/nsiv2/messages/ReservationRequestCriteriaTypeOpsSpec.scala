e6320779dba8716084166e132681b018260cd254
Bart Bakker <bart@thesoftwarecraft.com>
2016-11-03 17:36:15 +0100

    Use static java factories

--- src/test/scala/nl/surfnet/nsiv2/messages/ReservationRequestCriteriaTypeOpsSpec.scala
+++ src/test/scala/nl/surfnet/nsiv2/messages/ReservationRequestCriteriaTypeOpsSpec.scala
@@ -2,7 +2,7 @@ package nl.surfnet.nsiv2.messages
 
 import Generators._
 import javax.xml.datatype.XMLGregorianCalendar
-import nl.surfnet.bod.nsi.NillableCompanion
+import nl.surfnet.bod.nsi.Nillable
 import org.ogf.schemas.nsi._2013._12.connection.types.{ ReservationConfirmCriteriaType, ReservationRequestCriteriaType }
 
 class ReservationRequestCriteriaTypeOpsSpec extends org.specs2.mutable.Specification with org.specs2.ScalaCheck {
@@ -30,15 +30,15 @@ class ReservationRequestCriteriaTypeOpsSpec extends org.specs2.mutable.Specifica
       }
 
       "default missing start time to confirm criteria" in prop { (request: ReservationRequestCriteriaType, confirm: ReservationConfirmCriteriaType) =>
-        request.getSchedule.withStartTime(new NillableCompanion().absent[XMLGregorianCalendar])
+        request.getSchedule.withStartTime(Nillable.absent[XMLGregorianCalendar])
 
         request.toModifiedConfirmCriteria(confirm).get.getSchedule.getStartTime must_== confirm.getSchedule.getStartTime
       }
 
       "overwrite null end time" in prop { (request: ReservationRequestCriteriaType, confirm: ReservationConfirmCriteriaType) =>
-        request.getSchedule.withEndTime(new NillableCompanion().absent[XMLGregorianCalendar])
+        request.getSchedule.withEndTime(Nillable.absent[XMLGregorianCalendar])
 
-        request.toModifiedConfirmCriteria(confirm).get.getSchedule.getEndTime must_== new NillableCompanion().absent[XMLGregorianCalendar]
+        request.toModifiedConfirmCriteria(confirm).get.getSchedule.getEndTime must_== Nillable.absent[XMLGregorianCalendar]
       }
 
       "STPs" >> {
